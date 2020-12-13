c62558cb6c58f3883ec849ab28529cf9a58b076b
TolkachevPeter <peter.tolkachev@gmail.com>
2020-12-13 19:16:45 +0300

    add new interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { title } from 'process';
 import React, {useState} from 'react';
 import { Navbar } from './components/Navbar';
 import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
+import { ITodo } from './interfaces';
+
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState<ITodo[]>([])
 
   const addHandler = (title: string) => {
-    console.log('add new todo', title)
+    const newTodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    }
+    setTodos([newTodo, ...todos])
   }
 
   return( 
@@ -15,6 +23,8 @@ function App() {
   <Navbar/>
   <div className="container">
     <TodoForm onAdd = {addHandler}/>
+
+    <TodoList todos={todos}/>
   </div>
   </div>)
 }
